0ca7cac7b927a0cab660acd42dd0170c4294b33f
Giridhari Lal Gupta <giridhari.lal@students.iiit.ac.in>
2018-08-24 01:02:21 +0530

    -Fix bug with scrolling -Print Directory in reverse order -print selected directory content

--- Project/list_print.cpp
+++ Project/list_print.cpp
@@ -5,52 +5,40 @@ using namespace std;
 
 struct dirent ** namelist;
 
-string path = "/home/gunno/Documents";
-// struct dirlist {
-//     unsigned char  d_type;       type of file; not supported
-//                                    by all file system types 
-//     char           d_name[256]; /* filename */
-//     int pos;
-// };
-vector< pair<string,int> > v;
-vector< pair<string,int> >::iterator it;
+string path = "/home/gunno/Documents/os";
+
 
 int list_print(int cur_pos){
-	if(cur_pos < 2){
-		int itemCounter = 0;
+	// cout<<cur_pos<<" ";
+	int i = 0;
+	if(!cur_pos){
+		int itemCounter = 0,i=0;
 	    int n = scandir(path.c_str(),&namelist,NULL, alphasort);
-	    int i = 0;
-	    while(i < n){
-		    cout<<" "<<namelist[i]->d_name<<"\n";
+	    while(i<n){
+		    cout<<i<<" "<<namelist[i]->d_name<<"\n";
 		    i++;
-		    v.push_back(make_pair(namelist[i]->d_name,i));
 		    itemCounter++;
-		    // free(namelist[n]);
 	    }
-	    // free(namelist);
 	    return itemCounter;
 	}
-	int itemCounter = 0, i = 0;
-	it = v.begin(); string new_path;
-	while(it != v.end()){
-		pair<string,int> p = *it;
-		if(cur_pos == p.second){
-			if(namelist[i]->d_type == 'd'){
-				string s = namelist[i]->d_name;
-				new_path = s + '/' + path;
-			}
+	screen_point(0,0,1);
+	int itemCounter = 0;
+	string new_path;
+	// cout<<namelist[cur_pos-1]->d_name;
+	while(1){
+		if((int)namelist[cur_pos-1]->d_type == 4){
+			string s = namelist[cur_pos-1]->d_name;
+			new_path = path + '/' + s;
+			break;
 		}
-		else it++;
+		else break;
 	}
+	cout<<new_path<<" ";
 	int n = scandir(new_path.c_str(),&namelist,NULL, alphasort);
-	int j=0;
-	while(n--){
-		cout<<" "<<namelist[n]->d_name<<"\n";
-		j++;
-		v.push_back(make_pair(namelist[i]->d_name,i));
+	while(i < n){
+		cout<<" "<<namelist[i]->d_name<<"\n";
+		i++;
 		itemCounter++;
-		// free(namelist[n]);
 	}
-	// free(namelist);
-	return itemCounter;
+	pointer_move(itemCounter);
 }
--- Project/pointer_move.cpp
+++ Project/pointer_move.cpp
@@ -4,31 +4,27 @@ using namespace std;
 
 #define gotoxy(x,y) cout<<"\033["<<x<<";"<<y<<"H"
 
-// #define PASSWORD_LEN 8
 
 int pointer_move(int cur_pos){
 	struct termios initialrsettings, newrsettings;
-    // char password[PASSWORD_LEN + 1];
     tcgetattr(0, &initialrsettings);
     
     newrsettings = initialrsettings;
     newrsettings.c_lflag &= ~ECHO;
     newrsettings.c_lflag &= ~ICANON;
-    newrsettings.c_lflag &= ~ISIG;
+    // newrsettings.c_lflag &= ~ISIG;
     tcsetattr(0, TCSANOW, &newrsettings);
 
-    int c = 0, x = cur_pos+2, y = 0;
+    int c = 0, x = 1, y = 0;
     while(c != 101){
     		switch(c = getchar()){
-    		    case 'A': if(x > 2) screen_point(--x,y,0); break;
+    		    case 'A': if(x > 1) screen_point(--x,y,0); break;
     		    case 'B': if(x <= cur_pos) screen_point(++x,y,0); break;
-    		    case 'C': if(y < 144) screen_point(x,++y,0); break;
-    		    case 'D': if(y) screen_point(x,--y,0); break;
+    		    // case 'C': if(y < 144) screen_point(x,++y,0); break;
+    		    // case 'D': if(y) screen_point(x,--y,0); break;
+                case 10: list_print(x);
     		    default: break;
     	    }
-    }
-    if(getchar() == 10){
-    	return x;
     }
     tcsetattr(0, TCSANOW, &initialrsettings);
     exit(0);
